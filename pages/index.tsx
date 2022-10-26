a3430ee9edf6b2c5e4f5c112f81a4e136f880ada
Dominik <dominik.rubroeder@icloud.com>
2022-10-26 20:53:41 +0200

    Add Apple's color scheme for light and dark mode and also accent color

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Hello, world</h1>
+      <h1 className="text-accent">Hello, world</h1>
     </>
   );
 }
