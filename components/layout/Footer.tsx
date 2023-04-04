f7d3ff58ad3ce13c3102912b8c8d10bc532f1f6c
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-04-04 20:58:27 +0200

    Remove Downloads in Footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,72 +1,26 @@
 import React from 'react';
-import { ArrowDownTrayIcon } from '@heroicons/react/24/solid';
-
-const socialLinks = [
-  {
-    title: 'GitHub',
-    href: 'https://github.com/dominikrubroeder',
-    isExternal: true
-  }
-];
-
-const downloadLinks = [
-  {
-    title: 'CV',
-    href: '/'
-  }
-];
 
 const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
 
   return (
     <footer className="grid gap-2 p-4 text-center text-xs">
-      <div className="flex justify-between gap-4">
-        <div className="flex flex-1 items-start gap-2">
-          <span>Downloads:</span>
-          <ul>
-            {downloadLinks.map(({ title, href }, i) => (
-              <li key={i}>
-                <a
-                  href={href}
-                  className="interactive hover:text-black dark:hover:text-white"
-                  download
-                >
-                  <span className="flex items-start gap-1">
-                    {title}
-                    <ArrowDownTrayIcon className="h-4 w-4" />
-                  </span>
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div>
-          <span className="mr-2">&copy; {currentYear}</span>
-
+      <div className="flex items-center justify-center gap-4">
+        <div className="flex flex-wrap gap-1">
+          <div>
+            <span className="mr-1">&copy; {currentYear}</span>
+            <span className="text-black dark:text-white">Dominik Rubröder</span>
+          </div>
+          |
           <a
             href="https://github.com/dominikrubroeder"
             target="_blank"
             rel="noreferrer"
-            className=" text-black dark:text-white"
+            className="text-black dark:text-white"
+            title="GitHub | Dominik Rubröder"
           >
-            Dominik Rubröder
+            GitHub
           </a>
-        </div>
-
-        <div className="flex-1 text-right">
-          {socialLinks.map(({ title, href, isExternal }, i) => (
-            <a
-              key={i}
-              href={href}
-              className="interactive hover:text-black dark:hover:text-white"
-              target={isExternal ? '_blank' : '_self'}
-              rel="noreferrer"
-            >
-              {title}
-            </a>
-          ))}
         </div>
       </div>
     </footer>
