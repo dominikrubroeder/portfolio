c4556a87f3b170a14e05dada5acb1a484179dfb9
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-10-13 21:42:54 +0200

    Increase line height

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,10 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 const Document = () => {
   return (
     <Html lang="en" data-theme="dark" className="dark">
       <Head />
-      <body className="min-h-screen bg-theme-base text-theme-contrary/40">
+      <body className="min-h-screen bg-theme-base leading-[1.8] text-theme-contrary/40">
         <Main />
         <NextScript />
       </body>
