99d5662161d91809c49fa90748667227ba46f5ff
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-11-21 14:10:56 +0100

    Build Intro section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,26 +1,3 @@
 export default function Footer() {
-  const currentYear = new Date().getFullYear();
-
-  return (
-    <footer className="grid gap-2 p-4 text-center text-xs">
-      <div className="flex items-center justify-center gap-4">
-        <div className="flex flex-wrap gap-1">
-          <div>
-            <span className="mr-1">&copy; {currentYear}</span>
-            <span className="text-black dark:text-white">Dominik Rubröder</span>
-          </div>
-          |
-          <a
-            href="https://github.com/dominikrubroeder"
-            target="_blank"
-            rel="noreferrer"
-            className="text-black dark:text-white"
-            title="GitHub | Dominik Rubröder"
-          >
-            GitHub
-          </a>
-        </div>
-      </div>
-    </footer>
-  );
+  return <footer></footer>;
 }
