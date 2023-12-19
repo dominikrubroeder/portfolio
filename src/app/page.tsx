de48b8e179de8b552cad6a4fd67ae908aa09e29d
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-12-19 21:46:56 +0100

    Prototype sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { ChevronDoubleUpIcon } from '@heroicons/react/24/outline';
 export default async function HomePage() {
   return (
     <>
-      <section className="flex h-full items-center justify-center">
+      <section className="flex min-h-screen items-center justify-center">
         <div className="max-w-max">
           <h1 className="text-gradient bg-gradient-to-t from-apple-purple to-apple-pink text-8xl font-extrabold uppercase">
             Team frontend
@@ -12,14 +12,14 @@ export default async function HomePage() {
         </div>
       </section>
 
-      <section className="sticky bottom-0 p-4 w-full text-center border-white/10">
+      <section className="sticky top-2 p-4 w-full text-center border-white/10">
         <div className="mx-auto flex gap-4 items-center justify-center">
           <div className="flex items-center gap-4">
             <Image
               src="/avatar.jpg"
               alt="Dominik Rubröder Avatar"
-              width="72"
-              height="72"
+              width="64"
+              height="64"
               className="shrink-0 rounded-full border-white/10 bg-white/10 p-4"
             />
           </div>
@@ -28,11 +28,17 @@ export default async function HomePage() {
             <li>Technologies</li>
             <li>Socials</li>
           </ul>
-          <div className="flex items-center gap-4 justify-center bg-white/10 rounded-full h-20 w-20">
+          <div className="flex items-center gap-4 justify-center bg-white/10 rounded-full h-16 w-16">
             <ChevronDoubleUpIcon className="h-5 w-5" />
           </div>
         </div>
       </section>
+
+      <section className="min-h-screen"></section>
+
+      <section className="min-h-screen"></section>
+
+      <section className="min-h-screen"></section>
     </>
   );
 }
