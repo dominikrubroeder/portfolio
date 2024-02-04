d981e0d54a0280726b2e2f2146531d165ac20326
Dominik Rubröder <dominik.rubroeder@icloud.com>
2024-02-04 13:51:08 +0100

    feature(footer) – Remove unused spacing on desktop view at the bottom

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,8 +1,19 @@
 export default function Footer() {
   return (
-    <footer className="p-4 pb-24 text-center text-xs">
-      &copy; {new Date().getFullYear()}
-      <span className="ml-1.5 text-theme-contrary">Dominik Rubröder</span>
+    <footer className="p-4 pb-24 text-center text-xs md:pb-4">
+      <p>
+        <a
+          href="mailto:dominik.rubroeder@icloud.com"
+          title="Send mail to dominik.rubroeder@icloud.com"
+          aria-label="Send mail to dominik.rubroeder@icloud.com"
+        >
+          dominik.rubroeder@icloud.com
+        </a>
+      </p>
+      <p>
+        &copy; {new Date().getFullYear()}
+        <span className="ml-1.5 text-theme-contrary">Dominik Rubröder</span>
+      </p>
     </footer>
   );
 }
