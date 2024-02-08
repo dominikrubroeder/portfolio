319b9febf49f0c93ba0dc9721c20791c2155bf97
Dominik Rubröder <dominik.rubroeder@icloud.com>
2024-02-08 16:17:33 +0100

    feature(RowItem) – Larger `Tool` / `Technology` title for lower prioritized items

--- src/components/RowItem.tsx
+++ src/components/RowItem.tsx
@@ -7,7 +7,7 @@ import Brand from '@/components/Brand';
 
 export default function RowItem({ rowItem }: { rowItem: Tool | Technology }) {
   return (
-    <div className="grid gap-4 border-b border-gray-5 py-4 pr-4 text-xs sm:flex sm:items-start sm:justify-between md:items-center">
+    <div className="grid gap-4 border-b border-gray-5 py-4 pr-4 sm:flex sm:items-start sm:justify-between md:items-center">
       <div className="grid gap-4">
         <Link
           href={rowItem.website}
@@ -24,7 +24,7 @@ export default function RowItem({ rowItem }: { rowItem: Tool | Technology }) {
 
           <h3 className="font-bold text-theme-contrary">{rowItem.title}</h3>
 
-          <div className="mr-auto w-max whitespace-nowrap rounded-3xl border border-gray-4 px-3 py-2 font-bold text-theme-contrary">
+          <div className="mr-auto w-max whitespace-nowrap rounded-3xl border border-gray-4 px-3 py-2 text-xs font-bold text-theme-contrary">
             {rowItem.keyword}
           </div>
         </Link>
