1382deaee3af6a61427e5c98ef632d40bfb07e00
Dominik Rubröder <dominik.rubroeder@icloud.com>
2024-02-10 23:04:05 +0100

    feature(label) – Create reusable `label.tsx` Component

---
+++ src/components/label.tsx
@@ -0,0 +1,17 @@
+export default function Label({
+  text,
+  className
+}: {
+  text: string;
+  className?: string;
+}) {
+  return (
+    <span
+      className={`mx-2 shrink-0 self-start rounded-sm bg-gray-5 p-1 px-1.5 text-sm text-theme-contrary ${
+        className ? className : null
+      }`}
+    >
+      {text}
+    </span>
+  );
+}
