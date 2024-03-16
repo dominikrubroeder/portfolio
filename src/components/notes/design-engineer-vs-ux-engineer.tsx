415d128ac8b06f1f5c93f7ed383d848b2849c04b
Dominik Rubröder <dominik.rubroeder@icloud.com>
2024-03-16 23:52:48 +0100

    fix – Color GitHub icon and external link identifier alignment

--- src/components/notes/design-engineer-vs-ux-engineer.tsx
+++ src/components/notes/design-engineer-vs-ux-engineer.tsx
@@ -5,16 +5,16 @@ export default function DesignEngineerVsUxEngineer() {
   return (
     <Link
       href="https://chat.openai.com/share/e54e4594-ea73-4d2f-93c8-31b89b6b2232"
-      className="mx-auto flex cursor-pointer select-none items-center justify-between gap-4 rounded-2xl bg-gray-5 px-4 py-3 text-violet-400 transition hover:bg-gray-4"
+      className="mx-auto flex cursor-pointer select-none items-center gap-2 rounded-2xl bg-gray-5 px-4 py-3 text-violet-400 transition hover:bg-gray-4"
       target="_blank"
       title="View 'Design Engineer vs UX Engineer' on ChatGPT"
       aria-label="View 'Design Engineer vs UX Engineer' on ChatGPT"
     >
+      <ArrowTopRightOnSquareIcon className="h-4 w-4 text-gray-2" />
       <h2 className="font-bold">
         Design Engineer <span className="font-normal text-gray-2">vs</span> UX
         Engineer
       </h2>
-      <ArrowTopRightOnSquareIcon className="h-4 w-4 text-gray-2" />
     </Link>
   );
 }
