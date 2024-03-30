c5ba5ad8ee0fcd39aa8038aba65cb8a5af20bc63
Dominik Rubröder <dominik.rubroeder@icloud.com>
2024-03-30 18:22:38 +0100

    feature(AccordionMini) – Add `accordion-mini.tsx` component to collapse/hide content in a more minimalistic and barebone way than `accordion.tsx`

---
+++ src/components/ui/accordion-mini.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { ReactNode, useState } from 'react';
+import { ChevronDownIcon } from '@heroicons/react/24/outline';
+import { clsx } from 'clsx';
+
+export default function AccordionMini({
+  title,
+  children
+}: {
+  title: string;
+  children: ReactNode;
+}) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div>
+      <div
+        className="flex cursor-pointer select-none justify-between gap-4"
+        title="Open accordion to see more content"
+        aria-label="Open accordion to see more content"
+        onClick={() => setOpen((prevState) => !prevState)}
+      >
+        <h3 className="font-bold text-theme-contrary">{title}</h3>
+
+        <ChevronDownIcon
+          className={clsx(
+            'h-4 w-4 text-gray-2 transition',
+            open ? '-rotate-90' : 'rotate-0'
+          )}
+        />
+      </div>
+
+      {open ? <div className="mb-5 animate-fadeUp">{children}</div> : null}
+    </div>
+  );
+}
