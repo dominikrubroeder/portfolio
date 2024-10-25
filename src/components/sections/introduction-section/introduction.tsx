b0f7e9542e84c72d834c7ef15f69e231d61c70ce
Dominik Rubröder <dominik.rubroeder@icloud.com>
2024-10-25 21:11:10 +0200

    feature(Texting) – Correct some texting issues

--- src/components/sections/introduction-section/introduction.tsx
+++ src/components/sections/introduction-section/introduction.tsx
@@ -4,7 +4,7 @@ import AccordionClean from '@/components/atoms/accordion-clean';
 import Mindset from '@/components/sections/introduction-section/mindset';
 import Books from '@/components/sections/introduction-section/books';
 import TechStack from '@/components/sections/introduction-section/tech-stack';
-import Link from '@/components/atoms/link';
+import Vision from '@/components/sections/introduction-section/vision';
 
 export default function Introduction({ className }: { className?: string }) {
   return (
@@ -20,49 +20,7 @@ export default function Introduction({ className }: { className?: string }) {
       </h3>
 
       <AccordionClean title="More about my vision">
-        <div className="space-y-4">
-          <p>
-            As a UX Design Engineer, i am working for good user interfaces that
-            transport emotion and do care about the impact of design. I want the
-            user to enjoy their actions, feeling pleasant and delightful while
-            scrolling through the page, pursuing his or her actions with joyful
-            ease.
-          </p>
-
-          <p>
-            With smooth animations and transitions that support the user&apos;s
-            behaviour and provide guidance, the application should feel
-            interactive, yet stable. Supporting the user to achieve their goals
-            and intentions on the site seamlessly.
-          </p>
-
-          <p>
-            I like to use
-            <Link
-              href="https://lawsofux.com"
-              title="Open Laws of UX from Jon Yablonski about psychological principles for UX and Design"
-              aria-label="Open Laws of UX from Jon Yablonski about psychological principles for UX and Design"
-            >
-              psychological and design principles
-            </Link>
-            to build better and stable
-            <Link
-              href="https://nextjs.org/docs/app/building-your-application/deploying/production-checklist"
-              title="Open production checklist on the Next.js documenation"
-              aria-label="Open production checklist on the Next.js documenation"
-              className="ml-0"
-            >
-              production
-            </Link>
-            applications.
-          </p>
-
-          <p>
-            I think when the idea and the vision is clear, you can build the
-            best product possible, with the right people, and the right
-            technology.
-          </p>
-        </div>
+        <Vision />
       </AccordionClean>
 
       <AccordionClean title="Things i like">
