4ddd1fccdb8bcd48d09237cd1fa2a46ae80bf9e7
Dominik Rubröder <dominik.rubroeder@icloud.com>
2024-12-29 22:29:00 +0100

    Change texting for `home-hero-section`

--- src/components/organisms/home-hero-section.tsx
+++ src/components/organisms/home-hero-section.tsx
@@ -1,7 +1,6 @@
 import React, { Suspense } from 'react';
 import ControlBar from '@/components/molecules/control-bar/control-bar';
 import { ControlBarSections } from '@/types';
-import Button from '@/components/atoms/button';
 
 const sections: ControlBarSections = [
   {
@@ -57,39 +56,11 @@ export default function HomeHeroSection() {
       >
         <div className="max-w-screen-md space-y-4 px-4">
           <h2 className="text-6xl font-bold text-foreground sm:text-8xl">
-            For the Frontend
+            Team Frontend
           </h2>
           <p className="mx-1 text-4xl font-bold leading-relaxed text-primary sm:text-6xl">
-            Building applications by
-            <code className="relative ml-2 inline-block">
-              code
-              <small className="absolute -top-3 right-10 rounded border bg-muted px-0.5 text-xs">
-                .ts
-              </small>
-              <small className="absolute -top-3 right-0 rounded border bg-muted px-0.5 text-xs">
-                .tsx
-              </small>
-            </code>
-            ,
-            <span className="relative mx-2 -mt-4 inline-block rounded border">
-              <span className="absolute -left-1 -top-1 size-2 rounded-full bg-primary" />
-              <span className="absolute -right-1 -top-1 size-2 rounded-full bg-primary" />
-              <span className="absolute -bottom-1 -left-1 size-2 rounded-full bg-primary" />
-              <span className="absolute -bottom-1 -right-1 size-2 rounded-full bg-primary" />
-              design
-            </span>
-            and psychological principles
+            Build your vision
           </p>
-        </div>
-
-        <div className="flex w-full max-w-screen-md gap-2 px-5">
-          <Button variant="primary" className="hidden sm:block">
-            Download CV
-          </Button>
-
-          <Button variant="outline" className="hidden sm:block">
-            Get to know me
-          </Button>
         </div>
       </section>
 
