f3b26706872e51eadb39ff4421c2dab668b5e482
Dominik Rubröder <dominik.rubroeder@icloud.com>
2025-05-09 12:26:40 +0200

    feat(UI2) – Rebuild Hero Section for UI2

--- src/components/design-showcase.tsx
+++ src/components/design-showcase.tsx
@@ -8,20 +8,18 @@ import { DesignDrawer } from '@/components/organisms/design-drawer';
 export function DesignShowcase() {
   const [state, setState] = useState<{
     enabled: boolean;
-    color: 'primary' | 'neutral' | 'white' | 'black' | undefined;
+    color: 'primary' | 'neutral' | undefined;
   }>({
     enabled: false,
-    color: undefined
+    color: 'neutral'
   });
 
   return (
     <span
       className={cn(
-        'group relative inline-block border transition hover:bg-muted sm:mx-4',
+        'group relative inline-block border transition hover:border-primary hover:bg-muted sm:mx-4',
         state.color === 'primary' && 'text-primary',
-        state.color === 'neutral' && 'text-foreground',
-        state.color === 'white' && 'bg-muted text-white',
-        state.color === 'black' && 'text-black'
+        state.color === 'neutral' && 'text-foreground'
       )}
       onClick={() =>
         setState((prevState) => {
@@ -35,19 +33,7 @@ export function DesignShowcase() {
       <span className="absolute -bottom-1 -left-1 size-2 bg-primary group-hover:animate-spin" />
       <span className="absolute -right-1 -bottom-1 size-2 bg-primary group-hover:animate-spin" />
       <DesignDrawer className="absolute -right-3.5 -bottom-3 text-primary sm:hidden" />
-      <div className="invisible absolute -top-12 left-1/2 flex -translate-x-1/2 gap-2 opacity-0 transition-all duration-300 group-hover:visible group-hover:-translate-y-2 group-hover:opacity-100">
-        <span
-          className="flex size-8 shrink-0 interactive items-center justify-center rounded-full border bg-primary hover:border-2 hover:border-primary"
-          onClick={() =>
-            setState((prevState) => {
-              return { ...prevState, color: 'primary' };
-            })
-          }
-        >
-          {state.color === 'primary' && (
-            <CheckIcon className="size-4 shrink-0 text-primary-foreground" />
-          )}
-        </span>
+      <div className="absolute -top-10 left-1/2 flex -translate-1/2 gap-2">
         <span
           className="flex size-8 shrink-0 interactive items-center justify-center rounded-full border bg-foreground hover:border-2 hover:border-primary"
           onClick={() =>
@@ -61,34 +47,22 @@ export function DesignShowcase() {
           )}
         </span>
         <span
-          className="flex size-8 shrink-0 interactive items-center justify-center rounded-full border bg-white hover:border-2 hover:border-primary"
+          className="flex size-8 shrink-0 interactive items-center justify-center rounded-full border bg-primary hover:border-2 hover:border-primary"
           onClick={() =>
             setState((prevState) => {
-              return { ...prevState, color: 'white' };
+              return { ...prevState, color: 'primary' };
             })
           }
         >
-          {state.color === 'white' && (
-            <CheckIcon className="size-4 shrink-0 text-black" />
-          )}
-        </span>
-        <span
-          className="flex size-8 shrink-0 interactive items-center justify-center rounded-full border bg-black hover:border-2 hover:border-primary"
-          onClick={() =>
-            setState((prevState) => {
-              return { ...prevState, color: 'black' };
-            })
-          }
-        >
-          {state.color === 'black' && (
-            <CheckIcon className="size-4 shrink-0 text-white" />
+          {state.color === 'primary' && (
+            <CheckIcon className="size-4 shrink-0 text-primary-foreground" />
           )}
         </span>
 
         <DesignDrawer className="hidden text-muted-foreground sm:flex" />
       </div>
       {state.color && (
-        <span className="absolute top-1 right-1 text-xs">
+        <span className="absolute top-1.5 right-1 text-[.6rem]">
           <span className="hidden sm:inline-block">Color:</span> {state.color}
         </span>
       )}
--- src/components/organisms/introduction/introduction.tsx
+++ src/components/organisms/introduction/introduction.tsx
@@ -27,16 +27,12 @@ export function Introduction() {
           <div className="text-wrap">
             <b className="mr-1.5 text-foreground">
               My work is committed to the frontend. I like to create and build
-              dynamic interfaces that are designed to be for a joyful and
-              interactive web
+              dynamic interfaces that are designed to be for an interactive web.
             </b>
-            <p>
-              – later on, native mobile apps might join that paradigm. I am a
-              designer that like&#39;s to code to make the design available to
-              everyone.
-            </p>
-
-            <p>Design, code or psychological principles helps to</p>
+            <span>
+              I am a designer that enjoys to code to make the design available
+              to everyone.
+            </span>
           </div>
 
           <ul className="space-y-6">
