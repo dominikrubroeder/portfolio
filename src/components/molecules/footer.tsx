9028c0be8edb380eb9eb2a64efea79fbd98b2462
Dominik Rubröder <dominik.rubroeder@icloud.com>
2025-05-30 21:38:59 +0200

    feat(UI2) – Upgrade `next` to version 15.3.3

--- src/components/molecules/footer.tsx
+++ src/components/molecules/footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
       <p>Live your balance.</p>
       <p className="space-x-2">
         <a href="https://nextjs.org" target="_blank" rel="nofollow">
-          Next.js 15.3.1
+          Next.js 15.3.3
         </a>
         <span>|</span>
         <a
@@ -19,7 +19,7 @@ export function Footer() {
         </a>
         <span>|</span>
         <a href="https://tailwindcss.com/docs/installation/using-vite">
-          Tailwind CSS 4.1.4
+          Tailwind CSS 4.1.7
         </a>
       </p>
       <p>
