ec0f321046ae3f73129496e8c7ab2b752465ab8d
Dominik Rubröder <dominik.rubroeder@icloud.com>
2025-06-01 17:19:07 +0200

    feat(UI2) – Prepare SEO metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,25 @@ import { AppProvider } from '@/components/organisms/app';
 
 export const metadata: Metadata = {
   title: 'Dominik Rubröder | UX Design Engineer',
-  description: 'Building dynamic user interfaces for the web.'
+  description:
+    'Create and build dynamic user interfaces that are designed to be for an interactive web.',
+  openGraph: {
+    title: 'Dominik Rubröder | UX Design Engineer',
+    description:
+      'Create and build dynamic user interfaces that are designed to be for an interactive web.',
+    url: 'https://www.dominikrubroeder.dev',
+    siteName: 'dominikrubroeder.dev',
+    images: [
+      {
+        url: 'https://www.dominikrubroeder.dev/avatar.jpg',
+        width: 500,
+        height: 500,
+        alt: 'Profile image Dominik Rubröder'
+      }
+    ],
+    locale: 'de_DE',
+    type: 'website'
+  }
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
