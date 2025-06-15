3047e09f74a3135b3e3a44889eb7861105521588
Dominik Rubröder <dominik.rubroeder@icloud.com>
2025-06-15 18:44:33 +0200

    feat(Projects) – Build project view UI

--- src/components/organisms/projects/projects.tsx
+++ src/components/organisms/projects/projects.tsx
@@ -1,165 +1,15 @@
-import { Badge } from '@/components/atoms/badge';
-import { MediawaveLogo } from '@/components/atoms/logo';
 import Link from 'next/link';
-import { Button } from '@/components/atoms/button';
+
 import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/solid';
-import { technologies } from '@/components/organisms/technologies';
+import { Badge } from '@/components/atoms/badge';
+import { Button } from '@/components/atoms/button';
 import { Ul } from '@/components/atoms/ul';
 import { Brand } from '@/components/atoms/brand';
-import { tools } from '@/components/organisms/tools';
-
-const projects = [
-  {
-    title: 'Value Booster',
-    category: ['Headless', 'Ecommerce'],
-    url: 'https://www.mediawave.de/value-booster',
-    readableTitle: 'Kick start in your headless ecommerce project',
-    status: 'Currently developing',
-    logo: <MediawaveLogo className="h-14 w-auto" />,
-    role: ['Frontend Development', 'Design Consulting'],
-    aspects: [
-      'Build user interface based on design files',
-      'Connect several headless systems into one centric user interface',
-      'Maintain and build up design system to ensure flexibility and reusability'
-    ],
-    tools: [
-      tools
-        .find((technologyGroup) =>
-          technologyGroup.children.find(
-            (technology) => technology.title === 'Figma'
-          )
-        )
-        ?.children.find((technology) => technology.title === 'Figma'),
-      tools
-        .find((technologyGroup) =>
-          technologyGroup.children.find(
-            (technology) => technology.title === 'WebStorm'
-          )
-        )
-        ?.children.find((technology) => technology.title === 'WebStorm'),
-      tools
-        .find((technologyGroup) =>
-          technologyGroup.children.find(
-            (technology) => technology.title === 'Shopware'
-          )
-        )
-        ?.children.find((technology) => technology.title === 'Shopware'),
-      tools
-        .find((technologyGroup) =>
-          technologyGroup.children.find(
-            (technology) => technology.title === 'Storyblok'
-          )
-        )
-        ?.children.find((technology) => technology.title === 'Storyblok'),
-      tools
-        .find((technologyGroup) =>
-          technologyGroup.children.find(
-            (technology) => technology.title === 'Algolia'
-          )
-        )
-        ?.children.find((technology) => technology.title === 'Algolia'),
-      tools
-        .find((technologyGroup) =>
-          technologyGroup.children.find(
-            (technology) => technology.title === 'Auth.js'
-          )
-        )
-        ?.children.find((technology) => technology.title === 'Auth.js'),
-      tools
-        .find((technologyGroup) =>
-          technologyGroup.children.find(
-            (technology) => technology.title === 'Jira'
-          )
-        )
-        ?.children.find((technology) => technology.title === 'Jira'),
-      tools
-        .find((technologyGroup) =>
-          technologyGroup.children.find(
-            (technology) => technology.title === 'Teams'
-          )
-        )
-        ?.children.find((technology) => technology.title === 'Teams'),
-      tools
-        .find((technologyGroup) =>
-          technologyGroup.children.find(
-            (technology) => technology.title === 'GitLab'
-          )
-        )
-        ?.children.find((technology) => technology.title === 'GitLab'),
-      tools
-        .find((technologyGroup) =>
-          technologyGroup.children.find(
-            (technology) => technology.title === 'Craft'
-          )
-        )
-        ?.children.find((technology) => technology.title === 'Craft')
-    ],
-    technologies: [
-      technologies
-        .find((technologyGroup) =>
-          technologyGroup.children.find(
-            (technology) => technology.title === 'Next.js'
-          )
-        )
-        ?.children.find((technology) => technology.title === 'Next.js'),
-      technologies
-        .find((technologyGroup) =>
-          technologyGroup.children.find(
-            (technology) => technology.title === 'React'
-          )
-        )
-        ?.children.find((technology) => technology.title === 'React'),
-      technologies
-        .find((technologyGroup) =>
-          technologyGroup.children.find(
-            (technology) => technology.title === 'TypeScript'
-          )
-        )
-        ?.children.find((technology) => technology.title === 'TypeScript'),
-      technologies
-        .find((technologyGroup) =>
-          technologyGroup.children.find(
-            (technology) => technology.title === 'Tailwind CSS'
-          )
-        )
-        ?.children.find((technology) => technology.title === 'Tailwind CSS')
-    ]
-  },
-  {
-    title: 'sautershop',
-    category: ['DIY workers'],
-    url: 'https://www.sautershop.de',
-    readableTitle: 'Home wood-working tools',
-    status: '2024 – Today'
-  },
-  {
-    title: 'HD+',
-    category: ['TV'],
-    url: 'https://www.hd-plus.de',
-    readableTitle: 'Everything you need for good television',
-    status: '2021 - 2023'
-  },
-  {
-    title: 'Segmüller',
-    category: ['Furniture'],
-    url: 'https://www.segmueller.de',
-    readableTitle: 'Home furniture from Germany',
-    status: '2021 - 2023'
-  },
-  {
-    title: 'alles auto',
-    category: ['Car dealer'],
-    url: 'https://www.alles.auto',
-    readableTitle: '',
-    status: '2021'
-  }
-];
+import { projects } from '@/components/organisms/projects/data';
 
 export function Projects() {
   const project = projects[0];
-
-  console.log(project.tools);
-
+  
   return (
     <div className="space-y-6">
       <h2>
@@ -230,40 +80,48 @@ export function Projects() {
             <div className="space-y-4">
               <b className="block">Tools</b>
               <ul className="flex flex-wrap gap-6">
-                {project.tools?.map((tool, index) => (
-                  <li key={index}>
-                    <Link
-                      href={tool.website}
-                      title={tool.title}
-                      aria-label={`Open ${tool.title} website`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <span className="sr-only">{tool.title}</span>
-                      <Brand brand={tool.title} className="size-12" />
-                    </Link>
-                  </li>
-                ))}
+                {project.tools?.map((tool, index) => {
+                  if (tool && tool.title) {
+                    return (
+                      <li key={index}>
+                        <Link
+                          href={tool.website}
+                          title={tool.title}
+                          aria-label={`Open ${tool.title} website`}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <span className="sr-only">{tool.title}</span>
+                          <Brand brand={tool.title} className="size-12" />
+                        </Link>
+                      </li>
+                    );
+                  }
+                })}
               </ul>
             </div>
 
             <div className="space-y-4">
               <b className="block">Technologies</b>
               <ul className="flex flex-wrap gap-6">
-                {project.technologies?.map((technology, index) => (
-                  <li key={index}>
-                    <Link
-                      href={technology.website}
-                      title={technology.title}
-                      aria-label={`Open ${technology.title} website`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <span className="sr-only">{technology.title}</span>
-                      <Brand brand={technology.title} className="size-12" />
-                    </Link>
-                  </li>
-                ))}
+                {project.technologies?.map((technology, index) => {
+                  if (technology && technology.title) {
+                    return (
+                      <li key={index}>
+                        <Link
+                          href={technology.website}
+                          title={technology.title}
+                          aria-label={`Open ${technology.title} website`}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <span className="sr-only">{technology.title}</span>
+                          <Brand brand={technology.title} className="size-12" />
+                        </Link>
+                      </li>
+                    );
+                  }
+                })}
               </ul>
             </div>
           </div>
