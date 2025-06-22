064273ca051c4e3c9db4606a9c61df7a16914a14
Dominik Rubröder <dominik.rubroeder@icloud.com>
2025-06-22 19:32:10 +0200

    feat(Texting) – Refine texting in `home-hero`, `tools` and projects `data`

--- src/components/organisms/home/home-hero.tsx
+++ src/components/organisms/home/home-hero.tsx
@@ -1,15 +1,18 @@
 import { Container } from '@/components/atoms/container';
+import { Badge } from '@/components/atoms/badge';
 
 export function HomeHero() {
   return (
-    <Container
-      className="flex flex-col items-center justify-center pt-4 pb-1 lg:pb-2"
-      id="hero"
-    >
-      <h2 className="space-y-8 sm:max-w-(--readable-container)">
-        Building user interfaces for the web. A craft, about UI components and
-        design systems.
-      </h2>
+    <Container className="flex flex-col items-center justify-center pt-5 pb-1 lg:pb-2">
+      <div className="space-y-2">
+        <Badge className="font-bold tracking-wider text-primary">
+          Team Frontend
+        </Badge>
+        <h2 className="space-y-8 sm:max-w-(--readable-container)">
+          Building user interfaces for the web. A craft, about UI components and
+          design systems.
+        </h2>
+      </div>
     </Container>
   );
 }
