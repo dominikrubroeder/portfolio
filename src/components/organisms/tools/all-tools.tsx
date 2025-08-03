d9c5b7f18c97275604ef0bd5cb95efa0419032d6
Dominik Rubröder <dominik.rubroeder@icloud.com>
2025-08-03 15:34:55 +0200

    feat(Tools) – Add button style "ghost" to tool brand logos

--- src/components/organisms/tools/all-tools.tsx
+++ src/components/organisms/tools/all-tools.tsx
@@ -23,54 +23,53 @@ export function AllTools() {
                   <h3 className="mb-0 text-base">{item.group}</h3>
 
                   {index === 0 && (
-                    <span className="absolute top-1 right-4 text-xs xs:right-18 sm:right-21">
+                    <span className="absolute top-1 right-4 text-xs sm:right-22.5">
                       Experience level
                     </span>
                   )}
                 </div>
 
-                <ul className="space-y-5 rounded border p-4">
+                <ul className="space-y-4 rounded border p-4">
                   {item.children
                     .sort((a, b) => a.title.localeCompare(b.title))
                     .map((child, index) => (
-                      <li key={index} className="relative flex gap-3 xs:gap-4">
+                      <li
+                        key={index}
+                        className="relative flex justify-between gap-3 pr-2 xs:gap-4"
+                      >
                         <Link
                           href={child.website}
                           target="_blank"
-                          rel="noopener noreferrer"
+                          className="group flex items-center gap-3 xs:gap-4"
                         >
-                          <BrandLogo
-                            brand={child.title}
-                            group={item.group}
-                            className="size-10 shrink-0"
-                          />
+                          <Button
+                            variant="ghost"
+                            className="size-14 [&>svg]:size-10"
+                          >
+                            <BrandLogo
+                              brand={child.title}
+                              group={item.group}
+                              className="size-10"
+                            />
+                          </Button>
+
+                          <h3 className="mb-0 max-w-24 self-center overflow-hidden text-base text-ellipsis whitespace-nowrap xs:max-w-30 sm:max-w-none">
+                            {child.title}
+                          </h3>
                         </Link>
 
-                        <div className="flex flex-1 items-center justify-between gap-2 overflow-hidden">
-                          <Link
+                        <div className="flex items-center justify-between gap-2 bg-background sm:gap-4">
+                          <ExperienceBar progress={child.knowledge} />
+
+                          <Button
+                            variant="ghost"
                             href={child.website}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="group"
+                            title={`Go to external ${child.title} website`}
+                            aria-label={`Go to external ${child.title} website`}
+                            className="hidden group-hover:after:hidden sm:flex"
                           >
-                            <h3 className="mb-0 max-w-24 overflow-hidden text-base text-ellipsis whitespace-nowrap xs:max-w-30 sm:max-w-none">
-                              {child.title}
-                            </h3>
-                          </Link>
-
-                          <div className="flex items-center justify-between gap-2 bg-background sm:gap-4">
-                            <ExperienceBar progress={child.knowledge} />
-
-                            <Button
-                              variant="ghost"
-                              href={child.website}
-                              title={`Go to external ${child.title} website`}
-                              aria-label={`Go to external ${child.title} website`}
-                              className="hidden sm:flex"
-                            >
-                              <ArrowTopRightOnSquareIcon className="size-5" />
-                            </Button>
-                          </div>
+                            <ArrowTopRightOnSquareIcon className="size-5" />
+                          </Button>
                         </div>
                       </li>
                     ))}
