2e9c275e4d2073daa4eb72bcb8b629eec5906a3d
Dominik Rubröder <dominik.rubroeder@icloud.com>
2025-08-03 15:40:27 +0200

    feat(Footer) – Update package versions in footer component

--- src/components/molecules/footer.tsx
+++ src/components/molecules/footer.tsx
@@ -12,7 +12,7 @@ export function Footer() {
           title="Open Next.js website"
           aria-label="Open Next.js documentation"
         >
-          Next.js 15.3.5
+          Next.js 15.4.5
         </ExternalLink>
 
         <span>|</span>
@@ -34,7 +34,7 @@ export function Footer() {
           title="Open Tailwind CSS documentation"
           aria-label="Open Tailwind CSS documentation"
         >
-          Tailwind CSS 4.1.10
+          Tailwind CSS 4.1.11
         </ExternalLink>
       </p>
 
