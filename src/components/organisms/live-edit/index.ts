576c0de3b846e2737595c5bebf9065d6eff2b442
Dominik Rubröder <dominik.rubroeder@icloud.com>
2025-12-12 08:38:56 +0100

    feat(LiveEdit) – Continue live-edit feature

--- src/components/organisms/live-edit/index.ts
+++ src/components/organisms/live-edit/index.ts
@@ -2,5 +2,6 @@ export { LiveEditProvider, useLiveEdit } from './live-edit-context';
 export { LiveEditContainer } from './live-edit-container';
 export { LiveEditDrawer } from './live-edit-drawer';
 export { LiveEditToggle } from './live-edit-toggle';
+export { LiveEditToolbar } from './live-edit-toolbar';
 
 export * from './types';
