f407efdd29f4db170a55b211a4d241a754854726
Dominik Rubröder <dominik.rubroeder@icloud.com>
2025-12-12 08:43:57 +0100

    feat(brand-link) – Show `label` permanently, when `showLabel` is enabled

--- src/components/organisms/brand/brand-link.tsx
+++ src/components/organisms/brand/brand-link.tsx
@@ -24,30 +24,23 @@ export function BrandLink({
       className={cn(
         'group relative',
         size === 'small' && 'p-3 [&>img]:size-8 [&>svg]:size-8',
-        size === 'medium' && 'p-4 [&>img]:size-12 [&>svg]:size-12'
+        size === 'medium' && 'p-4 [&>img]:size-12 [&>svg]:size-12',
+        showLabel &&
+          (labelPosition === 'top' || labelPosition === 'bottom') &&
+          'grid items-center justify-center text-center',
+        showLabel &&
+          (labelPosition === 'left' || labelPosition === 'right') &&
+          'flex items-center',
+        showLabel && 'gap-3'
       )}
     >
-      {showLabel && (
-        <span
-          className={cn(
-            'invisible absolute z-50 w-max rounded-full bg-muted px-3 py-1 text-center text-xs text-foreground opacity-0 group-hover:visible group-hover:opacity-100',
-            labelPosition === 'top' &&
-              'top-0 translate-y-0 group-hover:-translate-y-9',
-            labelPosition === 'bottom' &&
-              'bottom-0 translate-y-0 group-hover:translate-y-9',
-            labelPosition === 'left' &&
-              'top-1/2 -left-3 -translate-x-full -translate-y-1/2',
-            labelPosition === 'right' &&
-              'top-1/2 -right-3 translate-x-full -translate-y-1/2'
-          )}
-        >
-          {brand.name}
-        </span>
+      {!!brand.logo ? (
+        brand.logo
+      ) : (
+        <BrandLogo brand={brand.name} className="mx-auto" />
       )}
 
-      {!!brand.logo ? brand.logo : <BrandLogo brand={brand.name} />}
-
-      <span className="sr-only">Open {brand.name}</span>
+      <span className={cn(!showLabel && 'sr-only')}>{brand.name}</span>
     </Button>
   );
 }
