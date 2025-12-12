60b25edb73d1d7a58fa6389dc9f075e7d45c5452
Dominik Rubröder <dominik.rubroeder@icloud.com>
2025-12-12 08:40:39 +0100

    feat(LiveEdit) – Continue live-edit feature

--- src/components/organisms/live-edit/types.ts
+++ src/components/organisms/live-edit/types.ts
@@ -1,10 +1,19 @@
 import { Dispatch, ReactNode, SetStateAction } from 'react';
 
+export type LiveEditElement = {
+  id: string;
+  name: string;
+} | null;
+
 export type LiveEditContextType = {
   liveEditEnabled: boolean;
   setLiveEditEnabled: Dispatch<SetStateAction<boolean>>;
+  isOpenToolbar: boolean;
+  setIsOpenToolbar: Dispatch<SetStateAction<boolean>>;
   isOpenDrawer: boolean;
   setIsOpenDrawer: Dispatch<SetStateAction<boolean>>;
+  liveEditElement: LiveEditElement;
+  setLiveEditElement: (liveEditElement: LiveEditElement) => void;
 };
 
 export type LiveEditContextProviderProps = {
