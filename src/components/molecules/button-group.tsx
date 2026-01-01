e070bb0501dd72106e947dea06ae8d7de0a6c2b4
Dominik Rubröder <dominik.rubroeder@icloud.com>
2026-01-01 18:19:25 +0100

    feat(ButtonGroup) – `minimizeSpacing` for tighter `button` layout

--- src/components/molecules/button-group.tsx
+++ src/components/molecules/button-group.tsx
@@ -7,10 +7,12 @@ import { useTheme } from '@/components/organisms/theme';
 
 export function ButtonGroup({
   stretchButtons,
+  minimizeSpacing,
   className,
   children
 }: {
   stretchButtons?: boolean;
+  minimizeSpacing?: boolean;
   className?: string;
   children: ReactNode;
 }) {
@@ -22,6 +24,7 @@ export function ButtonGroup({
         'flex flex-wrap items-center gap-2',
         stretchButtons &&
           '[&>a]:flex-1 sm:[&>a]:flex-none [&>button]:flex-1 sm:[&>button]:flex-none',
+        minimizeSpacing && 'gap-0 -space-x-1',
         theme.key === 'wireframe' && className?.includes('gap-0') && 'gap-2!',
         className
       )}
