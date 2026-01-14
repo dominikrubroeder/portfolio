d4a3100287e8f4b8cf2c29b4ddc1c708395ac47b
Dominik Rubröder <dominik.rubroeder@icloud.com>
2026-01-14 21:53:47 +0100

    feat(UI) – Prepare UI 2.1

---
+++ src/components/organisms/what/what.tsx
@@ -0,0 +1,18 @@
+import { Container } from '@/components/atoms/container';
+import { formatDate } from '@/lib/utils';
+
+export function What() {
+  return (
+    <Container id="what" className="space-y-4">
+      <h2>What does the user interface work mean to me?</h2>
+
+      <p>
+        Very much with creativity. Seeing and identifing the brand with its
+        values and principles, and building the user interfaces based on that.
+        In addition to that, combine the user interface with core design
+        principles, psychological design patterns and the tools and technologies
+        of today.
+      </p>
+    </Container>
+  );
+}
