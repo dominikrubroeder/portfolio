c0685910925cd92e2c51a61c675adb757d8deb9e
Dominik Rubröder <dominik.rubroeder@icloud.com>
2026-01-30 20:14:24 +0100

    feat(Components) – Create `home-template` for the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,5 @@
-import { Container } from '@/components/atoms/container';
-import { ContactSection } from '@/components/organisms/contact';
-import { FAQ } from '@/components/organisms/faq';
-import { HomeHero } from '@/components/organisms/home';
-import { Projects } from '@/components/organisms/projects';
-import { Socials } from '@/components/organisms/socials';
-import { Technologies } from '@/components/organisms/technologies';
-import { Tools } from '@/components/organisms/tools';
-import {
-  EVENTS_WINTER_SCENE_ENABLED,
-  Snowfall
-} from '@/components/organisms/events';
-import {
-  ThemeNotesBackgroundCornellNotes,
-  ThemeWireframeBackground
-} from '@/components/organisms/theme';
+import { HomeTemplate } from '@/components/templates/home-template';
 
 export default async function HomePage() {
-  return (
-    <Container
-      as="div"
-      size="container"
-      horizontalPadding
-      className="space-y-24"
-    >
-      {EVENTS_WINTER_SCENE_ENABLED && <Snowfall />}
-
-      <ThemeWireframeBackground />
-
-      <ThemeNotesBackgroundCornellNotes />
-
-      <HomeHero />
-
-      <Projects />
-
-      <Tools />
-
-      <Technologies />
-
-      <Socials />
-
-      <FAQ />
-
-      <ContactSection />
-    </Container>
-  );
+  return <HomeTemplate />;
 }
---
+++ src/components/templates/home-template.tsx
@@ -0,0 +1,47 @@
+import {
+  EVENTS_WINTER_SCENE_ENABLED,
+  Snowfall
+} from '@/components/organisms/events';
+import {
+  ThemeNotesBackgroundCornellNotes,
+  ThemeWireframeBackground
+} from '@/components/organisms/theme';
+import { HomeHero } from '@/components/organisms/home';
+import { Projects } from '@/components/organisms/projects';
+import { Tools } from '@/components/organisms/tools';
+import { Technologies } from '@/components/organisms/technologies';
+import { Socials } from '@/components/organisms/socials';
+import { FAQ } from '@/components/organisms/faq';
+import { ContactSection } from '@/components/organisms/contact';
+import { Container } from '@/components/atoms/container';
+
+export function HomeTemplate() {
+  return (
+    <Container
+      as="div"
+      size="container"
+      horizontalPadding
+      className="space-y-16 sm:space-y-24"
+    >
+      {EVENTS_WINTER_SCENE_ENABLED && <Snowfall />}
+
+      <ThemeWireframeBackground />
+
+      <ThemeNotesBackgroundCornellNotes />
+
+      <HomeHero />
+
+      <Projects />
+
+      <Tools />
+
+      <Technologies />
+
+      <Socials />
+
+      <FAQ />
+
+      <ContactSection />
+    </Container>
+  );
+}
