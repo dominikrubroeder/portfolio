a7f342ab8daf96863638f88d4dd3c72d63ea7fa1
Dominik Rubröder <dominik.rubroeder@icloud.com>
2026-02-03 20:41:36 +0100

    feat(UI) – Display hero background in UI patterns and shapes style

--- src/components/organisms/theme/theme-wireframe-background.tsx
+++ src/components/organisms/theme/theme-wireframe-background.tsx
@@ -1,12 +1,7 @@
 'use client';
 
 import { Shapes } from '@/components/atoms/svg';
-import { useTheme } from '@/components/organisms/theme';
 
 export function ThemeWireframeBackground() {
-  const { theme } = useTheme();
-
-  if (theme.key !== 'wireframe') return null;
-
-  return <Shapes className="absolute top-28 left-0 z-0 animate-fade-up-1rem" />;
+  return <Shapes className="absolute top-28 left-0 z-0 hidden xl:block" />;
 }
