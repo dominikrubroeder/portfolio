5d99173e01dbfb1f52efe1e7e9a47ea56deee241
Dominik Rubröder <dominik.rubroeder@icloud.com>
2026-02-23 18:14:53 +0100

    feat(Logos) – Create unified directory for .png logos

--- src/components/atoms/logo/img/logo-procreate.tsx
+++ src/components/atoms/logo/img/logo-procreate.tsx
@@ -1,5 +1,10 @@
 import { cn } from '@/lib/utils';
-import { defaultLogoHeight, defaultLogoSquareSize, defaultLogoWidth, LogoProps } from '@/components/atoms/logo';
+import {
+  defaultLogoHeight,
+  defaultLogoSquareSize,
+  defaultLogoWidth,
+  LogoProps
+} from '@/components/atoms/logo';
 import Image from 'next/image';
 
 export function LogoProcreate({ id, className }: LogoProps) {
