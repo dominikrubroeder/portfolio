7c2c7f5aaa0f954dc3d08bc1165a3518d4d3d2d3
Dominik Rubröder <dominik.rubroeder@icloud.com>
2026-03-16 17:00:13 +0100

    feat(UI) – Prepare mobile SVG icons

--- src/components/organisms/home/home-hero.tsx
+++ src/components/organisms/home/home-hero.tsx
@@ -1,6 +1,7 @@
 import { Container } from '@/components/atoms/container';
 import { Animate } from '@/components/molecules/animate/animate';
 import { Introduction } from '@/components/organisms/introduction';
+import { SvgCircle, SvgRectangle, SvgTriangle } from '@/components/atoms/svg';
 
 export function HomeHero() {
   return (
@@ -11,9 +12,17 @@ export function HomeHero() {
         id="hero"
       >
         <div className="xs:space-y-2">
-          <div className="space-x-1.5 text-primary">
-            <span>The art of</span>
-            <span className="font-handwritten text-2xl">composing</span>
+          <div className="flex justify-between gap-2">
+            <div className="space-x-1.5 text-primary">
+              <span>The art of</span>
+              <span className="font-handwritten text-2xl">composing</span>
+            </div>
+
+            <div className="hidden">
+              <SvgCircle className="hover:scale-105 motion-safe:interactive motion-safe:transition" />
+              <SvgRectangle className="rounded hover:rounded-none motion-safe:interactive motion-safe:transition" />
+              <SvgTriangle className="hover:-rotate-90 motion-safe:interactive motion-safe:transition" />
+            </div>
           </div>
 
           <h2 className="bg-background">
