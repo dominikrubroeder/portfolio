f9de95bc9e8ebc12029dddfaddb096b83b42c2cb
Dominik Rubröder <dominik.rubroeder@icloud.com>
2026-03-16 17:04:40 +0100

    feat(Projects) – Update projects related data set

--- src/components/organisms/projects/types.ts
+++ src/components/organisms/projects/types.ts
@@ -4,20 +4,26 @@ import type { Brand } from '@/components/organisms/brand';
 
 export interface Project {
   title: string;
+  subline?: string;
   category: string[];
   url: string;
   isCurrent: boolean;
   isActive: boolean;
-  readableTitle: string;
+  description: string;
   timeframe?: string;
   logo: ReactNode | undefined;
   role: string[] | undefined;
   aspects: string[] | undefined;
   tools: Brand[] | undefined;
   technologies: Brand[] | undefined;
-  company: string | undefined;
+  companies:
+    | {
+        href: string | undefined;
+        label: 'mediawave commerce GmbH' | 'Loy GmbH' | 'Private';
+      }[]
+    | undefined;
   caseStudyUrls: string[] | undefined;
-  demoUrls: string[] | undefined;
+  demoUrls: { label: string; src: string }[] | undefined;
   previewImage?: string;
   previewImageTimestamp?: string;
 }
