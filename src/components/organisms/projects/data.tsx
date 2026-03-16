f643ba5ecf992ccf7e26a4a7c848a8dc4c15cfc2
Dominik Rubröder <dominik.rubroeder@icloud.com>
2026-03-16 16:34:12 +0100

    feat(Projects) – Update projects related data set

--- src/components/organisms/projects/data.tsx
+++ src/components/organisms/projects/data.tsx
@@ -3,6 +3,7 @@ import {
   LogoFrontendMentor,
   LogoGithub,
   LogoHdPlus,
+  LogoIu,
   LogoLoyGmbh,
   LogoMediawave,
   LogoSautershop,
@@ -10,43 +11,47 @@ import {
 } from '@/components/atoms/logo';
 import type { Project } from '@/components/organisms/projects/types';
 import { getTechnologies } from '@/components/organisms/technologies';
-import { getTools } from '@/components/organisms/tools';
+import { getTools } from '@/components/organisms/tools'; // TODO: Complete tools and technologie logos
 
 // TODO: Complete tools and technologie logos
 export const projects: Project[] = [
   {
-    title: 'uvex',
+    title: 'Uvex',
     category: ['Headless', 'B2C', 'Ecommerce'],
     url: 'https://uvex-group.shop',
     isCurrent: true,
     isActive: true,
-    readableTitle:
-      'Protecting People – The mission of the uvex brand with its vison and with its products',
+    description:
+      'Protecting People – The mission of the Uvex brand with its vison and with its products',
     timeframe: 'From 2025 – Ongoing',
     logo: <LogoSautershop id="projects-sautershop" className="h-12 w-auto" />,
     role: ['Design Engineer'],
-    aspects: undefined,
+    aspects: ['Consulting in CMS component set integration in Storyblok'],
     tools: getTools({
       names: [
         'Figma',
         'WebStorm',
-        'Shopware',
+        'Adobe Commerce',
         'Storyblok',
-        'Algolia',
-        'Auth.js',
+        'Storybook',
+        'FactFinder',
         'Jira',
         'Slack',
         'Teams',
         'GitLab',
-        'Motion',
         'GitLab'
       ],
-      sortBy: 'like-input'
+      sortBy: 'A-Z'
     }),
     technologies: getTechnologies({
-      names: ['Next.js', 'React', 'TypeScript', 'Tailwind CSS']
+      names: ['React', 'TypeScript']
     }),
-    company: 'mediawave commerce GmbH',
+    companies: [
+      {
+        href: 'https://www.mediawave.de',
+        label: 'mediawave commerce GmbH'
+      }
+    ],
     caseStudyUrls: undefined,
     demoUrls: undefined,
     previewImage: '/projects/project-uvex-preview-wallpaper.png',
@@ -58,8 +63,7 @@ export const projects: Project[] = [
     url: 'https://www.sautershop.com/en',
     isCurrent: false,
     isActive: false,
-    readableTitle:
-      'Quality woodworking tools and accessorizes for DIY projects',
+    description: 'Quality woodworking tools and accessorizes for DIY projects',
     timeframe: 'From 2024 – Feb. 2026',
     logo: <LogoSautershop id="projects-sautershop" className="h-12 w-auto" />,
     role: ['Frontend Developer'],
@@ -79,12 +83,17 @@ export const projects: Project[] = [
         'Motion',
         'GitLab'
       ],
-      sortBy: 'like-input'
+      sortBy: 'A-Z'
     }),
     technologies: getTechnologies({
       names: ['Next.js', 'React', 'TypeScript', 'Tailwind CSS']
     }),
-    company: 'mediawave commerce GmbH',
+    companies: [
+      {
+        href: 'https://www.mediawave.de',
+        label: 'mediawave commerce GmbH'
+      }
+    ],
     caseStudyUrls: undefined,
     demoUrls: undefined,
     previewImage: '/projects/project-sautershop-preview-wallpaper.png',
@@ -94,25 +103,26 @@ export const projects: Project[] = [
     title: 'Value Booster',
     category: ['Headless', 'B2B', 'B2C', 'Ecommerce'],
     url: 'https://www.mediawave.de/value-booster',
-    readableTitle:
+    description:
       'Kickstart in your headless ecommerce project with the mediawave Value Booster',
     isCurrent: true,
     isActive: true,
     timeframe: 'From 2024 – Ongoing',
     logo: <LogoMediawave id="projects-mediawave" className="h-12 w-auto" />,
-    role: ['Frontend Developer', 'Design Consulting'],
+    role: ['Frontend Designer', 'Frontend Developer', 'Design Consultant'],
     aspects: [
       'Building the user interface with/without design files',
       'Connecting several headless systems into one centric user interface',
-      'Maintaining and building up the design system to ensure consistency, flexibility and reusability'
+      'Maintain and build up the design system to ensure a consistent, flexible and reusable user interface'
     ],
     tools: getTools({
       names: [
         'Figma',
         'WebStorm',
+        'Junie',
         'Shopware',
         'Storyblok',
-        'Algolia',
+        'meilisearch',
         'Auth.js',
         'Jira',
         'Slack',
@@ -121,12 +131,17 @@ export const projects: Project[] = [
         'Motion',
         'GitLab'
       ],
-      sortBy: 'like-input'
+      sortBy: 'A-Z'
     }),
     technologies: getTechnologies({
       names: ['Next.js', 'React', 'TypeScript', 'Tailwind CSS']
     }),
-    company: 'mediawave commerce GmbH',
+    companies: [
+      {
+        href: 'https://www.mediawave.de',
+        label: 'mediawave commerce GmbH'
+      }
+    ],
     caseStudyUrls: undefined,
     demoUrls: undefined,
     previewImage: '/projects/project-value-booster-preview-wallpaper.png',
@@ -136,7 +151,7 @@ export const projects: Project[] = [
     title: 'HD+',
     category: ['Headless', 'B2C', 'Ecommerce'],
     url: 'https://www.hd-plus.de',
-    readableTitle:
+    description:
       'Watch television in HD — via satellite or internet (IP), on your TV and smartphone',
     isCurrent: false,
     isActive: false,
@@ -159,14 +174,21 @@ export const projects: Project[] = [
         'Motion',
         'GitLab'
       ],
-      sortBy: 'like-input'
+      sortBy: 'A-Z'
     }),
     technologies: getTechnologies({
       names: ['Next.js', 'React', 'TypeScript', 'Tailwind CSS']
     }),
-    company: 'mediawave commerce GmbH',
+    companies: [
+      {
+        href: 'https://www.mediawave.de',
+        label: 'mediawave commerce GmbH'
+      }
+    ],
     caseStudyUrls: undefined,
-    demoUrls: ['https://www.hd-plus.de'],
+    demoUrls: [
+      { label: 'https://www.hd-plus.de', src: 'https://www.hd-plus.de' }
+    ],
     previewImage: '/projects/project-hd-plus-preview-wallpaper.png',
     previewImageTimestamp: '2026-01-06'
   },
@@ -174,7 +196,7 @@ export const projects: Project[] = [
     title: 'Segmüller',
     category: ['Ecommerce', 'B2C'],
     url: 'https://www.segmueller.de',
-    readableTitle: 'Well-known home furniture store in Augsburg (Germany)',
+    description: 'Well-known home furniture store in Augsburg (Germany)',
     isCurrent: false,
     isActive: false,
     timeframe: 'From 2021 – 2023',
@@ -191,14 +213,25 @@ export const projects: Project[] = [
         'Slack',
         'Figma',
         'GitLab'
-      ]
+      ],
+      sortBy: 'A-Z'
     }),
     technologies: getTechnologies({
-      names: ['Twig', 'HTML', 'CSS', 'Sass']
+      names: ['Twig', 'HTML', 'CSS', 'JavaScript', 'Sass']
     }),
-    company: 'mediawave commerce GmbH',
+    companies: [
+      {
+        href: 'https://www.mediawave.de',
+        label: 'mediawave commerce GmbH'
+      }
+    ],
     caseStudyUrls: ['https://www.mediawave.de/case-study/segmuller'],
-    demoUrls: ['https://www.segmueller.de'],
+    demoUrls: [
+      {
+        label: 'https://www.segmueller.de',
+        src: 'https://www.segmueller.de'
+      }
+    ],
     previewImage: '/projects/project-segmueller-preview-wallpaper.png',
     previewImageTimestamp: '2026-01-06'
   },
@@ -206,7 +239,7 @@ export const projects: Project[] = [
     title: 'alles.auto Service Portal',
     category: ['B2B'],
     url: 'https://www.alles.auto',
-    readableTitle: 'Online selling and re-selling and car service platform',
+    description: 'Online selling and re-selling and car service platform',
     isCurrent: false,
     isActive: false,
     timeframe: '2021',
@@ -216,24 +249,69 @@ export const projects: Project[] = [
     role: ['Frontend Developer'],
     aspects: undefined,
     tools: getTools({
-      names: ['VS Code', 'Inkdrop', 'Jira', 'Teams', 'Slack', 'Figma', 'GitLab']
+      names: [
+        'VS Code',
+        'Inkdrop',
+        'Jira',
+        'Teams',
+        'Slack',
+        'Figma',
+        'GitLab'
+      ],
+      sortBy: 'A-Z'
     }),
     technologies: getTechnologies({
       names: ['React', 'CSS', 'Sass']
     }),
-    company: 'mediawave commerce GmbH',
+    companies: [
+      {
+        href: 'https://www.mediawave.de',
+        label: 'mediawave commerce GmbH'
+      }
+    ],
     caseStudyUrls: ['https://www.mediawave.de/case-study/allesauto'],
-    demoUrls: ['https://www.alles.auto']
+    demoUrls: [
+      { label: 'https://www.alles.auto', src: 'https://www.alles.auto' }
+    ]
+  },
+  {
+    title: 'IU International University of Applied Sciences',
+    subline: "Bachelor's degree in Media Design",
+    category: [],
+    url: 'https://www.iu.de/en/bachelor/mediendesign/',
+    description:
+      '"Design your life. With your Media Design studies". I studied Media Design at the IU International University of Applied Sciences in Munich, with specialization in user experience. While study, we had several theoretical, but also practical projects – targeting mostly digital media, but also print media, audio and video related projects.',
+    timeframe: 'From 2019 – 2023',
+    role: ['Dual Student'],
+    isCurrent: false,
+    isActive: false,
+    logo: <LogoIu id="projects-iu" className="size-32" />,
+    aspects: undefined,
+    tools: undefined,
+    technologies: undefined,
+    companies: [
+      {
+        href: 'https://www.mediawave.de',
+        label: 'mediawave commerce GmbH'
+      },
+      {
+        href: 'https://www.loy.info',
+        label: 'Loy GmbH'
+      }
+    ],
+    caseStudyUrls: undefined,
+    demoUrls: []
   },
   {
     title: 'Loy GmbH',
+    subline: 'Trainee as Media Designer in Design and Tech',
     category: ['B2C', 'B2B', 'Ecommerce', 'Website'],
     url: 'https://www.loy.info',
-    readableTitle:
-      'A small digital agency for small and mid-sized companies nearby Munich, Bavaria. Here I spend my two and a half years of training, three additional years during dual study at the IU International University of Applied Sciences, Munich and a full-time job. It was all about digital media work, development of UI and layout, and in some parts about print media.',
+    description:
+      'A small digital agency for small and mid-sized companies nearby Munich, Bavaria. Here I spend my two and a half years of training, three additional years during dual study at the IU International University of Applied Sciences (Munich) and a full-time position. My work was all about digital media, design and development of user interfaces, layouts, and in some parts about print media.',
     isCurrent: false,
     isActive: false,
-    timeframe: 'From 2016 – 2021',
+    timeframe: 'From 2016 – 2019',
     logo: <LogoLoyGmbh id="projects-loy-gmbh" className="h-12 w-auto" />,
     role: [
       'Trainee',
@@ -256,25 +334,38 @@ export const projects: Project[] = [
         'Shopware',
         'WordPress',
         'Contenido'
-      ]
+      ],
+      sortBy: 'A-Z'
     }),
     technologies: getTechnologies({
       names: ['HTML', 'CSS', 'JavaScript', 'Twig']
     }),
-    company: 'Loy GmbH',
+    companies: undefined,
     caseStudyUrls: undefined,
     demoUrls: [
-      'https://www.loy.info/referenzen-internetagentur/',
-      'https://www.locaboo.com',
-      'https://www.welleat.de',
-      'https://variasophia.de'
+      {
+        label: 'https://www.loy.info/referenzen-internetagentur/',
+        src: 'https://www.loy.info/referenzen-internetagentur/'
+      },
+      {
+        label: 'https://www.locaboo.com',
+        src: 'https://www.locaboo.com'
+      },
+      {
+        label: 'https://www.welleat.de',
+        src: 'https://www.welleat.de'
+      },
+      {
+        label: 'https://variasophia.de',
+        src: 'https://variasophia.de'
+      }
     ]
   },
   {
     title: 'Frontend Mentor',
     category: [],
     url: 'https://www.frontendmentor.io',
-    readableTitle: 'Many frontend coding challenges using a real-life workflow',
+    description: 'Many frontend coding challenges using a real-life workflow.',
     isCurrent: false,
     isActive: false,
     logo: (
@@ -287,33 +378,86 @@ export const projects: Project[] = [
     aspects: undefined,
     tools: [],
     technologies: [],
-    company: undefined,
+    companies: undefined,
     caseStudyUrls: undefined,
     demoUrls: [
-      'https://github.com/dominikrubroeder?tab=repositories&q=frontendmentor',
-      'https://expenses-chart-component-website.vercel.app',
-      'https://intro-section-with-dropdown-navigation-website.vercel.app',
-      'https://ecommerce-product-page-website.vercel.app',
-      'https://interactive-pricing-component-website.vercel.app',
-      'https://space-tourism-website-dr.vercel.app',
-      'https://advice-generator-dr.vercel.app',
-      'https://github-user-search-app-dr.vercel.app',
-      'https://interactive-comments-section-dr.vercel.app',
-      'https://password-generator-app-dr.vercel.app',
-      'https://tip-calculator-app-dr.vercel.app',
-      'https://photosnap-multi-page-website-dr.vercel.app',
-      'https://entertainment-web-app-dr.vercel.app',
-      'https://dictionary-web-app-dr.vercel.app',
-      'https://skilled-elearning-landing-page-dr.vercel.app',
-      'https://sunnyside-agency-landing-page-dr.vercel.app',
-      'https://multi-step-form-dr.vercel.app'
+      {
+        label:
+          'https://github.com/dominikrubroeder?tab=repositories&q=frontendmentor',
+        src: 'https://github.com/dominikrubroeder?tab=repositories&q=frontendmentor'
+      },
+      {
+        label: 'https://expenses-chart-component-website.vercel.app',
+        src: 'https://expenses-chart-component-website.vercel.app'
+      },
+      {
+        label:
+          'https://intro-section-with-dropdown-navigation-website.vercel.app',
+        src: 'https://intro-section-with-dropdown-navigation-website.vercel.app'
+      },
+      {
+        label: 'https://ecommerce-product-page-website.vercel.app',
+        src: 'https://ecommerce-product-page-website.vercel.app'
+      },
+      {
+        label: 'https://interactive-pricing-component-website.vercel.app',
+        src: 'https://interactive-pricing-component-website.vercel.app'
+      },
+      {
+        label: 'https://space-tourism-website-dr.vercel.app',
+        src: 'https://space-tourism-website-dr.vercel.app'
+      },
+      {
+        label: 'https://advice-generator-dr.vercel.app',
+        src: 'https://advice-generator-dr.vercel.app'
+      },
+      {
+        label: 'https://github-user-search-app-dr.vercel.app',
+        src: 'https://github-user-search-app-dr.vercel.app'
+      },
+      {
+        label: 'https://interactive-comments-section-dr.vercel.app',
+        src: 'https://interactive-comments-section-dr.vercel.app'
+      },
+      {
+        label: 'https://password-generator-app-dr.vercel.app',
+        src: 'https://password-generator-app-dr.vercel.app'
+      },
+      {
+        label: 'https://tip-calculator-app-dr.vercel.app',
+        src: 'https://tip-calculator-app-dr.vercel.app'
+      },
+      {
+        label: 'https://photosnap-multi-page-website-dr.vercel.app',
+        src: 'https://photosnap-multi-page-website-dr.vercel.app'
+      },
+      {
+        label: 'https://entertainment-web-app-dr.vercel.app',
+        src: 'https://entertainment-web-app-dr.vercel.app'
+      },
+      {
+        label: 'https://dictionary-web-app-dr.vercel.app',
+        src: 'https://dictionary-web-app-dr.vercel.app'
+      },
+      {
+        label: 'https://skilled-elearning-landing-page-dr.vercel.app',
+        src: 'https://skilled-elearning-landing-page-dr.vercel.app'
+      },
+      {
+        label: 'https://sunnyside-agency-landing-page-dr.vercel.app',
+        src: 'https://sunnyside-agency-landing-page-dr.vercel.app'
+      },
+      {
+        label: 'https://multi-step-form-dr.vercel.app',
+        src: 'https://multi-step-form-dr.vercel.app'
+      }
     ]
   },
   {
     title: 'GitHub',
     category: [],
     url: 'https://github.com/dominikrubroeder?tab=repositories',
-    readableTitle:
+    description:
       'All the private projects i did over the last years. Not in specific order or priority. Some are removed for clean-up!',
     isCurrent: false,
     isActive: false,
@@ -322,8 +466,13 @@ export const projects: Project[] = [
     aspects: undefined,
     tools: [],
     technologies: [],
-    company: undefined,
+    companies: undefined,
     caseStudyUrls: undefined,
-    demoUrls: ['https://github.com/dominikrubroeder?tab=repositories']
+    demoUrls: [
+      {
+        label: 'https://github.com/dominikrubroeder?tab=repositories',
+        src: 'https://github.com/dominikrubroeder?tab=repositories'
+      }
+    ]
   }
 ];
