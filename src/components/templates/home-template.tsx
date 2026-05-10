9070f7b9ee5a404dc6dc74cb08dd373d263a09e2
Dominik Rubröder <dominik.rubroeder@icloud.com>
2026-05-10 12:15:15 +0200

    feat(UI) – Prepare UI 2.2.2

--- src/components/templates/home-template.tsx
+++ src/components/templates/home-template.tsx
@@ -22,7 +22,7 @@ export function HomeTemplate() {
 
       <ThemeNotesBackgroundCornellNotes />
 
-      <div className="mx-auto mb-6 max-w-(--container) bg-primary px-4 pb-4 text-4xl leading-[1.1] font-black text-primary-foreground xs:text-7xl sm:mb-8 sm:text-9xl sm:leading-none">
+      <div className="mx-auto -mt-4 mb-6 max-w-(--container) bg-primary px-4 pb-3.5 text-4xl leading-[1.1] font-black text-primary-foreground xs:text-7xl sm:mt-0 sm:mb-8 sm:pb-4 sm:text-9xl sm:leading-none">
         Design the thing. Code the thing. Make it good.
       </div>
     </>
