9cdc2d65077cc64c402a7861a93c3291941f00f9
Dominik Rubröder <54868035+dominikrubroeder@users.noreply.github.com>
2026-09-02 11:52:29 +0200

    Release UI 3.0.3 (#81)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Design with code, code by design | Dominik Rubröder, UX Engineer',
     description:
-      'This is my motto since i started my career. I love to design, even more with code.',
+      'Composing the frontend, its architecture, the user experience, the components, design systems, patterns, principles is my home to work with.',
     url: ROUTING_PUBLIC_DOMAIN,
     siteName: ROUTING_PUBLIC_DOMAIN.replace('https://www.', ''),
     images: [
