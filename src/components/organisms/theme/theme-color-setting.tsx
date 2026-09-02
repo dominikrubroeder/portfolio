aa09534e80e9d7dcb84b243378bc9a75f435ac91
Dominik Rubröder <54868035+dominikrubroeder@users.noreply.github.com>
2026-09-02 11:13:20 +0200

    Release UI 3.0.1 (#79)
    * 3.0.1
    
    * feat(UI3.0.1) – Align mobile viewport

--- src/components/organisms/theme/theme-color-setting.tsx
+++ src/components/organisms/theme/theme-color-setting.tsx
@@ -13,30 +13,30 @@ export function ThemeColorSetting({ className }: BaseComponentProps) {
     <ButtonGroup className={className}>
       {/** TODO: Replace fixed color values with color variables */}
       <button
-        className="flex size-8 shrink-0 items-center justify-center rounded-full border border-blue bg-blue hover:scale-110 hover:border-blue active:scale-100 [html[data-theme=default]_&]:motion-safe:interactive"
+        className="flex size-6.5 shrink-0 items-center justify-center rounded-full border border-blue bg-blue hover:scale-110 hover:border-blue active:scale-100 sm:size-8 [html[data-theme=default]_&]:motion-safe:interactive"
         onClick={() => setThemeColor('blue')}
       >
         {isInitialized && themeColor === 'blue' && (
-          <CheckIcon className="size-4 shrink-0 text-blue-foreground" />
+          <CheckIcon className="size-3.5 shrink-0 text-blue-foreground sm:size-4" />
         )}
       </button>
 
       <button
-        className="flex size-8 shrink-0 items-center justify-center rounded-full border-violett bg-violett hover:scale-110 hover:border-violett active:scale-100 [html[data-theme=default]_&]:motion-safe:interactive"
+        className="flex size-6.5 shrink-0 items-center justify-center rounded-full border-violett bg-violett hover:scale-110 hover:border-violett active:scale-100 sm:size-8 [html[data-theme=default]_&]:motion-safe:interactive"
         onClick={() => setThemeColor('violett')}
       >
         {((isInitialized && themeColor === 'primary') ||
           themeColor === 'violett') && (
-          <CheckIcon className="size-4 shrink-0 text-violett-foreground" />
+          <CheckIcon className="size-3.5 shrink-0 text-violett-foreground sm:size-4" />
         )}
       </button>
 
       <button
-        className="flex size-8 shrink-0 items-center justify-center rounded-full border border-orange bg-orange hover:scale-110 hover:border-orange active:scale-100 [html[data-theme=default]_&]:motion-safe:interactive"
+        className="flex size-6.5 shrink-0 items-center justify-center rounded-full border border-orange bg-orange hover:scale-110 hover:border-orange active:scale-100 sm:size-8 [html[data-theme=default]_&]:motion-safe:interactive"
         onClick={() => setThemeColor('orange')}
       >
         {isInitialized && themeColor === 'orange' && (
-          <CheckIcon className="size-4 shrink-0 text-orange-foreground" />
+          <CheckIcon className="size-3.5 shrink-0 text-orange-foreground sm:size-4" />
         )}
       </button>
     </ButtonGroup>
